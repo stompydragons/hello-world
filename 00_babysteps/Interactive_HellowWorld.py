5bf1ffd6d15dc4bc1a52e113776468b7074c2bf3
stompydragons <32965042+stompydragons@users.noreply.github.com>
2018-02-15 20:36:44 -0400

    Added Interactive_HellowWorld.py

---
+++ 00_babysteps/Interactive_HellowWorld.py
@@ -0,0 +1,6 @@
+myName = input('Please enter your name: ')
+myAge = input('Hi {}, what about your age: '.format(myName))
+
+print ('''Hello World.
+My name is {} and
+I am {} years old'''.format(myName, myAge))
